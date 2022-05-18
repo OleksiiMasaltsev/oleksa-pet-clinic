1a419b4ec6b46b73f88a852437e23c20728aaffb
Masaltsev <a.o.masaltsev@gmail.com>
2022-05-18 20:58:37 +0300

    init

---
+++ src/test/java/ua/masaltsev/oleksapetclinic/OleksaPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.masaltsev.oleksapetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OleksaPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
