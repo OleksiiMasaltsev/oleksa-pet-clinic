caa0789b1b6852bafd90599e5ae89c9fc6b0efc6
Masaltsev <a.o.masaltsev@gmail.com>
2022-05-22 01:23:45 +0300

    CrudService base interface. Closes #13

--- pet-clinic-data/src/main/java/ua/masaltsev/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/ua/masaltsev/petclinic/services/VetService.java
@@ -2,10 +2,6 @@ package ua.masaltsev.petclinic.services;
 
 import ua.masaltsev.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
