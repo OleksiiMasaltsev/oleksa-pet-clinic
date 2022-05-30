279855666dc451b1dd04f1ecf788eb4149961f2a
Masaltsev <a.o.masaltsev@gmail.com>
2022-05-30 20:24:18 +0300

    Spec, Vets entities added.

--- pet-clinic-data/src/main/java/ua/masaltsev/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/ua/masaltsev/petclinic/model/Speciality.java
@@ -1,6 +1,12 @@
 package ua.masaltsev.petclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
+
     private String description;
 
     public String getDescription() {
