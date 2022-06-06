afcadf3e0372e9318f455fd184e943a2ee1423c4
Masaltsev <a.o.masaltsev@gmail.com>
2022-06-06 22:37:41 +0300

    OwnerController all tests

--- pet-clinic-web/src/main/java/ua/masaltsev/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/ua/masaltsev/petclinic/controllers/OwnerController.java
@@ -1,13 +1,32 @@
 package ua.masaltsev.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ua.masaltsev.petclinic.model.Owner;
+import ua.masaltsev.petclinic.services.OwnerService;
+
+import java.util.Set;
 
 @Controller
+@RequestMapping("/owners")
 public class OwnerController {
 
-    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
-    public String index() {
+    private final OwnerService ownerService;
+
+    OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String index(Model model) {
+        Set<Owner> owners = ownerService.findAll();
+        model.addAttribute("owners", owners);
         return "owners/index";
+    }
+
+    @RequestMapping("/find")
+    public String find() {
+        return "not_implemented";
     }
 }
