1e9c8cc336c18dad80b0269d70e514498305ccf5
Masaltsev <a.o.masaltsev@gmail.com>
2022-06-20 19:11:20 +0300

    contextLoads passes

--- pet-clinic-web/src/test/java/oleksapetclinic/OleksaPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/oleksapetclinic/OleksaPetClinicApplicationTests.java
@@ -1,9 +1,10 @@
 package oleksapetclinic;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+//@SpringBootTest
+@ContextConfiguration
 class OleksaPetClinicApplicationTests {
 
 	@Test
