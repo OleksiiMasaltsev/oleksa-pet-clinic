04ac7c74cf6cc317d3ddf927c8a3fab6fa19311b
Masaltsev <a.o.masaltsev@gmail.com>
2022-06-25 00:13:41 +0300

    new templates with iFrame

--- pet-clinic-web/src/main/java/ua/masaltsev/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/ua/masaltsev/petclinic/controllers/VetController.java
@@ -1,13 +1,26 @@
 package ua.masaltsev.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ua.masaltsev.petclinic.model.Vet;
+import ua.masaltsev.petclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
 
+    private VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets.html", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        Set<Vet> vetSet = vetService.findAll();
+        model.addAttribute("vets", vetSet);
         return "vets/index";
     }
 }
